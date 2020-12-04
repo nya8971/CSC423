9d73837eedfc4a0b104319477ac6c733e236facb
nya8971 <nme25@miami.edu>
2020-12-04 07:41:52 -0500

    added python queries

---
+++ connect_oracle.py
@@ -0,0 +1,32 @@
+import cx_Oracle
+import pandas as pd
+
+"""
+Some quick start guides:
+* cx_Oracle 8: https://cx-oracle.readthedocs.io/en/latest/user_guide/installation.html
+* pandas: https://pandas.pydata.org/pandas-docs/stable/user_guide/10min.html
+"""
+# TODO change path of Oracle Instant Client to yours
+cx_Oracle.init_oracle_client(lib_dir = "./instantclient_19_8")
+
+# TODO change credentials
+# Connect as user "user" with password "mypass" to the "CSC423" service
+# running on lawtech.law.miami.edu
+connection = cx_Oracle.connect(
+    "nyeacsc423", "Nya8971!", "lawtech.law.miami.edu/CSC_423")
+cursor = connection.cursor()
+cursor.execute("""
+     SELECT h.hireNo, h.clientNo, h.registrationNo, h.firstName, h.lastName, h.homeAddress, h.phoneNum, h.mileageBefore, h.hireStartDate, h.mileageAfter,h.hireEndDate,v.model,v.make,v.capacity
+FROM HireAgreements h, Vehicles v
+WHERE (h.registrationNo = v.registrationNo) AND (v.capacity > 5)
+""")
+# get column names from cursor
+columns = [c[0] for c in cursor.description]
+# fetch data
+data = cursor.fetchall()
+# bring data into a pandas dataframe for easy data transformation
+df = pd.DataFrame(data, columns = columns)
+print(df) # examine
+print(df.columns)
+# print(df['FIRST_NAME']) # example to extract a colum
+connection.close()
